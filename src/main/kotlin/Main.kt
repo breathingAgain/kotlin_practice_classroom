4ec12be6c0e0addf683f2d2a05df744e608db6fd
stepan <derpq5@gmail.com>
2022-09-28 11:10:41 +0300

    first ex

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val numbers = listOf(1, 55, 25, 32, 2, 67, 31, 33, 11, 25, 66, 81, 97, 43, 46, 81)
+    sol1(numbers)
+    println("\n")
+    sol2(numbers)
+    println("\n")
+    sol3(numbers)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+}
+fun sol1(numbers: List<Int>){
+    for(item: Int in numbers){
+        if(item % 2 == 1)
+            print("$item ")
+    }
+}
+
+fun sol2(numbers: List<Int>){
+    for(item: Int in numbers){
+        if(item % 3 != 0)
+            print("$item ")
+    }
+}
+
+fun sol3(numbers: List<Int>){
+    for(i in numbers.indices){
+        if(numbers[i] % (i+1) == 0)
+            print("${numbers[i]} ")
+    }
 }
