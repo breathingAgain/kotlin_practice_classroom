c515955f22771b47575c4ca3582408d01f3d7aca
stepan <derpq5@gmail.com>
2022-09-28 11:52:21 +0300

    third ex

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,47 @@
+import java.lang.Math.pow
+import kotlin.math.sqrt
+
 fun main(args: Array<String>) {
-    val numbers = listOf(1, 55, 25, 32, 2, 67, 31, 33, 11, 25, 66, 81, 97, 43, 46, 81)
-    sol1(numbers)
-    println("\n")
-    sol2(numbers)
-}
-fun sol1(numbers: List<Int>){
-    numbers.forEach{
-        if (it % 2 == 1)
-            print("$it ")
+
+    data class Rectangle(val height: Int, val width: Int) {
+        val perimeter: Int
+            get() = height * 2 + width * 2
+        val area: Int
+            get() = height * width
     }
-}
-fun sol2(numbers: List<Int>){
-    numbers.forEach{
-        if (it % 3 != 0)
-            print("$it ")
+
+    val rectangles = listOf(
+        Rectangle(12, 1),
+        Rectangle(4, 6),
+        Rectangle(5, 15),
+        Rectangle(7, 32),
+        Rectangle(45, 32),
+        Rectangle(10, 12),
+        Rectangle(3, 9),
+    )
+
+    for(i  in rectangles){
+        print("${i.perimeter} ")
+    }
+    println()
+
+    for(i in rectangles){
+        print("${i.area} ")
+    }
+
+    println()
+
+    for(i in rectangles){
+        val a: Int = i.perimeter / 2
+        print("$a ")
+    }
+    println()
+
+    for (i in rectangles){
+        val a: Double = i.height.toDouble()
+        val b: Double = i.width.toDouble()
+        val c: Double = sqrt(pow(a, 2.0) + pow(b, 2.0))
+        print("$c ")
     }
 }
 
